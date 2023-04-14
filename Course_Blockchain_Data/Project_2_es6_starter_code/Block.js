efbafab26570c799a9f28f07ba56bdf24c0f4de3
Guilherme Brugger <guilherme.brugger@gmail.com>
2023-04-14 14:25:08 -0300

    Build first Blockchain

--- Course_Blockchain_Data/Project_2_es6_starter_code/Block.js
+++ Course_Blockchain_Data/Project_2_es6_starter_code/Block.js
@@ -3,10 +3,13 @@
 |  ===============================================*/
 
 class Block {
-	constructor(data){
-		// Add your Block properties
-		// Example: this.hash = "";
-	}
+  constructor(data) {
+    this.hash = "";
+    this.height = 0;
+    this.body = data;
+    this.time = 0;
+    this.previousblockhash = "";
+  }
 }
 
 module.exports.Block = Block;
